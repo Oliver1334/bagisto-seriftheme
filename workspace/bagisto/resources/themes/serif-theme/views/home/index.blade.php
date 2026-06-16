b3f3df43be6f3c1bd5631734c18ef26107282ca1
Oliver1334 <oliver.taylor95@outlook.com>
2026-06-16 23:35:03 +0100

    custom theme foundations

---
+++ workspace/bagisto/resources/themes/serif-theme/views/home/index.blade.php
@@ -0,0 +1,17 @@
+<x-shop::layouts>  
+    <x-slot:title>
+        Serif Theme Home
+    </x-slot>
+
+    <div class="container mx-auto mt-8 px-4 py-16">
+        <div class="text-center">
+            <h1 class="text-4xl font-bold text-gray-800 mb-4">
+                Welcome to Our Serif Theme
+            </h1>
+            
+            <p class="text-lg text-gray-600 mb-8">
+                This is your custom Bagisto store serif theme in action!
+            </p>
+        </div>
+    </div>
+</x-shop::layouts>
